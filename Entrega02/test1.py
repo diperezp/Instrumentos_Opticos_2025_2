91e780086f340fc54a4e96eb7da054a1e99635d0
diperezp <diperezp@unal.edu.co>
2025-11-01 00:51:12 -0500

    Add optical simulation script for image processing and propagation
    - Implemented a Python script (test4.py) for simulating optical propagation using LightPipes.
    - Imported and resized an image for processing.
    - Applied intensity modulation based on the image.
    - Propagated the optical field through various optical elements including lenses and circular screens.
    - Visualized the intensity distribution at different stages of propagation using matplotlib.

--- Entrega02/test1.py
+++ Entrega02/test1.py
@@ -10,31 +10,28 @@ pixel_size_test = 10e-6  # 10 micrómetros
 #instanciamos la clase ProcesamientoOptico
 optical_processor = ProcesamientoOptico(length_wave=lambda_test, pixel_size=pixel_size_test)
 
-#importamos una imagen del directorio actual
-image=optical_processor.import_transmitance(new_shape=(512,512),value_pad=0)
-
-print("Dimensiones de la transmitancia importada:", image.shape)
-
-#mostramos la transmitancia importada
+#cargamos la imagen del objeto
+optical_processor.import_transmitance((1024,1024),255)
+#mostramos la transmitancia
 optical_processor.show_transmitance()
-
-#realizamos la propagación de Fresnel a una distancia z=0.1 m
-z_propagation = 0.1  # 0.1 metros
-optical_processor.propagate(z=z_propagation, mode='fresnel')
-#mostramos el campo propagado
+#z de propagacion
+z=0.8 #1 cm
+optical_processor.propagate(z)
+#mostramos el campo
 optical_processor.show_propagated_field()
-#obtenemos el campo propagado
-field_propagated = optical_processor.get_field_propagated()
-#mostramos el campo propagado con matplotlib para verificar
-plt.imshow(np.angle(field_propagated), cmap='gray')
-plt.title('fase adquiridada (Fresnel)')
-plt.colorbar()
-plt.show()
+
+#ahora pasamos el campo atravez de una lente de foco
+f=0.2
+optical_processor.lens(f)
+
+#ahora propagamos el campo hasta el foco
+optical_processor.propagate(4/15)
+
+#mostramos el campo
+optical_processor.show_propagated_field()
 
 
-image=optical_processor.get_transmitance()
-#mostramos image con matplotlib para verificar
-plt.imshow(abs(image), cmap='gray')
-plt.title('Transmitancia Importada')
-plt.colorbar()
+image=optical_processor.get_field_propagated()
+fig=plt.figure(figsize=(10,12))
+plt.imshow(np.angle(image),cmap='gray')
 plt.show()
